a01e22d6ffff3afdf743ad4c18f817d7898d6240
= <otterlord.dev@gmail.com>
2023-08-07 14:13:06 +0100

    Remove astro-compress import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
