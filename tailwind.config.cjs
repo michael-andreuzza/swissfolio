c445d4289edde8e77ad8aa95d52cfd8fa8a3cc06
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-12-08 23:05:09 +0200

    Removed unused components - removed integrations - updated to v4 - change orange shade

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,13 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-
       colors: {
-        //white: "#8f9197",
-
         secondary: "#0000ff",
         primary: "#fafafa",
       },
       fontFamily: {
-        sans: ["Neue Haas Grotesk Text Pro", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-
 }
